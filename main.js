f89c611fef471fee436f338798ce4c525eb692d8
Chen, Luis <lurandal@hotmail.com>
2019-04-26 19:52:15 +0800

    Initial Menu items
    Initial commit for menu items.

--- main.js
+++ main.js
@@ -1,4 +1,51 @@
 const { app, BrowserWindow } = require('electron')
+const { Menu } = require('electron')
+
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      { label: 'Open File' },
+      { label: 'Close Files' },
+      { type: 'separator' },
+      { label: 'Export to JPEG' },
+      { label: 'Export to Excel' },
+      { type: 'separator' },
+      { role: 'Close' }      
+    ]
+  },
+  /* TODO: update menu items in future
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forcereload' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'close' }
+    ]
+  },
+  */
+  {
+    role: 'help',
+    submenu: [
+      {label: 'About'}
+    ]
+  }
+]
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 
 function createWindow () {   
   // 创建浏览器窗口
